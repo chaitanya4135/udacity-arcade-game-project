ed1c2e8c97e50447e68654ad7099803892e6abf1
chaitanya4135 <48980947+chaitanya4135@users.noreply.github.com>
2022-10-03 09:24:57 +0530

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -112,9 +112,10 @@ var Engine = (function(global) {
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
                 'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/grass-block.png',   // Row 2 of 3 of grass
+                'images/grass-block.png'    // Row 3 of 3 of grass
             ],
-            numRows = 6,
+            numRows = 7,
             numCols = 5,
             row, col;
 
